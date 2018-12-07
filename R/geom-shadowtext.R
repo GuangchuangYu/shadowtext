dea96950a3b854effae2f7a3bcf3a5bf89b131aa
Guangchuang Yu <guangchuangyu@gmail.com>
2018-12-07 14:01:13 +0800

    update docs #1

--- R/geom-shadowtext.R
+++ R/geom-shadowtext.R
@@ -22,7 +22,7 @@
 ##' @examples
 ##' library(ggplot2)
 ##' d <- data.frame(x = rnorm(3), y=rnorm(3), label = c('hello', 'world', '!!!'))
-##' ggplot(d, aes(x,y)) + geom_shadowtext(aes(label=label, color=label), bgcolor='firebrick')
+##' ggplot(d, aes(x,y)) + geom_shadowtext(aes(label=label, color=label), bg.colour='firebrick')
 ##' @export
 geom_shadowtext <- function(mapping = NULL, data = NULL,
                             stat = "identity", position = "identity",
